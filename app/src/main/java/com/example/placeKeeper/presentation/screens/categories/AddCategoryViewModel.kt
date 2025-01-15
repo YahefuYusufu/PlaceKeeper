47d6446afd1750f7675b5dc15802bd587cb6826f
YahefuYusufu <parguzatt@gmail.com>
2025-01-15 11:23:03 +0100

    everything has been done for create categoryviewmodel

---
+++ app/src/main/java/com/example/placeKeeper/presentation/screens/categories/AddCategoryViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.placeKeeper.presentation.screens.categories
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class AddCategoryViewModel @Inject constructor()
